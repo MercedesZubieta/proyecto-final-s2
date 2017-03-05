ed82a6d545570b325f43861816063c7e0d61c8af
MichelleSV <michelle@laboratoria.la>
2017-03-05 16:37:11 -0500

    Adding quiz correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,7 @@ window.onclick = function(event) {
 }
 // END DROPDOWN
 
+// MOSTRAR/OCULTAR SPRINT 1
 var sprints = document.getElementsByClassName("sprint");
 sprints[0].onclick= function quizOne() {
 	var divSprint1 = document.getElementById("sprint-1");
@@ -27,6 +28,8 @@ sprints[0].onclick= function quizOne() {
 		divSprint1.style.display = "none";
 	}
 }
+
+// MOSTRAR/OCULTAR SPRINT 2
 sprints[1].onclick= function quizOne() {
 	var divSprint2 = document.getElementById("sprint-2");
 	if (divSprint2.style.display == "none" ) {
@@ -34,4 +37,38 @@ sprints[1].onclick= function quizOne() {
 	} else {
 		divSprint2.style.display = "none";
 	}
+}
+
+//CORRECCION DE QUIZ SPRINT 1
+var opcionesQuiz1= document.getElementsByClassName("quiz-1");
+var contenedorQuiz1 = document.getElementById("contenedor-quiz1")
+document.getElementById("enviar-sprint1").onclick=function(){
+	var contadador = 0;
+	if(opcionesQuiz1[0].checked){
+		contadador++
+	}
+	if(opcionesQuiz1[4].checked){
+		contadador++
+	}
+	if(opcionesQuiz1[8].checked){
+		contadador++
+	}
+	contenedorQuiz1.innerHTML = "<p class='resultado-quiz'>Tienes " + contadador + " correctas"
+}
+
+//CORRECCION DE QUIZ SPRINT 1
+var opcionesQuiz2= document.getElementsByClassName("quiz-2");
+var contenedorQuiz2 = document.getElementById("contenedor-quiz2")
+document.getElementById("enviar-sprint2").onclick=function(){
+	var contadador = 0;
+	if(opcionesQuiz2[1].checked){
+		contadador++
+	}
+	if(opcionesQuiz2[3].checked){
+		contadador++
+	}
+	if(opcionesQuiz2[7].checked){
+		contadador++
+	}
+	contenedorQuiz2.innerHTML = "<p class='resultado-quiz'>Tienes " + contadador + " correctas"
 }
