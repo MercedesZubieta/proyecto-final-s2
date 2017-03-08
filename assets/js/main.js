1c7f4776c167a617ac454ed11a93bbf5db84a7a0
michellesegv <michelle@laboratoria.la>
2017-03-08 11:42:51 -0500

    change js, add quiz validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
+//BIENVENIDA A CODER
+var nombreCoder = document.getElementById("nombre-coder")
+if(localStorage.getItem("coder")){
+	nombreCoder.innerText=localStorage.getItem("coder");
+}
+else{
+	localStorage.setItem("coder",prompt("hola coder, ¿Cuál es tu nombre?"));
+	nombreCoder.innerText=localStorage.getItem("coder");
+}
+
+
 // DROPDOWN
-function myFunction() {
+function dropdown() {
 	document.getElementById("myDropdown").classList.toggle("show");
 }
 
 window.onclick = function(event) {
 	if (!event.target.matches('.dropbtn')) {
-
 		var dropdowns = document.getElementsByClassName("dropdown-content");
 		var i;
 		for (i = 0; i < dropdowns.length; i++) {
@@ -16,59 +26,93 @@ window.onclick = function(event) {
 		}
 	}
 }
-// END DROPDOWN
 
-// MOSTRAR/OCULTAR SPRINT 1
+
+// MOSTRAR/OCULTAR SPRINTS
+var s1 = document.getElementById("sprint-1");
+var s2 = document.getElementById("sprint-2");
+var s3 = document.getElementById("sprint-3");
 var sprints = document.getElementsByClassName("sprint");
-sprints[0].onclick= function quizOne() {
-	var divSprint1 = document.getElementById("sprint-1");
-	if (divSprint1.style.display == "none" ) {
-		divSprint1.style.display = "block";
-	} else {
-		divSprint1.style.display = "none";
-	}
+
+function mostrar(elemVisible) {
+	elemVisible.style.display = "block";
+}
+function ocultar(elemOculto1,elemOculto2) {
+	elemOculto1.style.display = "none";
+	elemOculto2.style.display = "none";
 }
 
-// MOSTRAR/OCULTAR SPRINT 2
-sprints[1].onclick= function quizOne() {
-	var divSprint2 = document.getElementById("sprint-2");
-	if (divSprint2.style.display == "none" ) {
-		divSprint2.style.display = "block";
-	} else {
-		divSprint2.style.display = "none";
-	}
+sprints[0].onclick= function(){
+	mostrar(s1);
+	ocultar(s2,s3);
 }
+sprints[1].onclick= function(){
+	mostrar(s2);
+	ocultar(s1,s3);
+}
+sprints[2].onclick= function(){
+	mostrar(s3);
+	ocultar(s1,s2);
+}
+
 
 //CORRECCION DE QUIZ SPRINT 1
 var opcionesQuiz1= document.getElementsByClassName("quiz-1");
 var contenedorQuiz1 = document.getElementById("contenedor-quiz1")
 document.getElementById("enviar-sprint1").onclick=function(){
-	var contadador = 0;
-	if(opcionesQuiz1[0].checked){
-		contadador++
+	var contestadas = 0;
+
+	console.log(opcionesQuiz1)
+	for(var i =0;i<9;i++){
+		if(opcionesQuiz1[i].checked==true){
+			contestadas++
+		}
 	}
-	if(opcionesQuiz1[4].checked){
-		contadador++
+	console.log(contestadas)
+	if(contestadas==3){
+		var correctas = 0;
+		if(opcionesQuiz1[0].checked){
+			correctas++
+		}
+		if(opcionesQuiz1[4].checked){
+			correctas++
+		}
+		if(opcionesQuiz1[8].checked){
+			correctas++
+		}
+		contenedorQuiz1.innerHTML = "<p class='resultado-quiz'>Tienes " + correctas + " correctas";
 	}
-	if(opcionesQuiz1[8].checked){
-		contadador++
+	else{
+		alert("Todas las preguntas son obligatorias")
 	}
-	contenedorQuiz1.innerHTML = "<p class='resultado-quiz'>Tienes " + contadador + " correctas"
 }
+
 
 //CORRECCION DE QUIZ SPRINT 1
 var opcionesQuiz2= document.getElementsByClassName("quiz-2");
 var contenedorQuiz2 = document.getElementById("contenedor-quiz2")
 document.getElementById("enviar-sprint2").onclick=function(){
-	var contadador = 0;
-	if(opcionesQuiz2[1].checked){
-		contadador++
+	var contestadas = 0;
+	for(var i =0;i<9;i++){
+		if(opcionesQuiz2[i].checked==true){
+			contestadas++
+		}
 	}
-	if(opcionesQuiz2[3].checked){
-		contadador++
+	if(contestadas==3){
+		var correctas = 0;
+		if(opcionesQuiz2[1].checked){
+			correctas++
+		}
+		if(opcionesQuiz2[3].checked){
+			correctas++
+		}
+		if(opcionesQuiz2[7].checked){
+			correctas++
+		}
+		contenedorQuiz2.innerHTML = "<p class='resultado-quiz'>Tienes " + correctas + " correctas";
 	}
-	if(opcionesQuiz2[7].checked){
-		contadador++
+	else{
+		alert("Todas las preguntas son obligatorias")
 	}
-	contenedorQuiz2.innerHTML = "<p class='resultado-quiz'>Tienes " + contadador + " correctas"
+
 }
